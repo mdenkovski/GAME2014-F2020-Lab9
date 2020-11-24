8f5206cf2fd186d579dd5b09af02f8761302a3fb
Michael Denkovski <michaeldenkovski@hotmail.com>
2020-11-24 13:18:00 -0500

    created health bar and health implementation along with respawn

---
+++ Assets/_Scripts/BarController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BarController : MonoBehaviour
+{
+
+    public Transform bar;
+    public Transform entity;
+    public int currentValue;
+    public int maxValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentValue = 100;
+        maxValue = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(entity != null)
+        {
+            transform.position = entity.position + Vector3.up * 0.8f;
+        }
+    }
+
+
+    public void SetValue(int newValue)
+    {
+        currentValue = newValue;
+        bar.localScale = new Vector3((float)((double)currentValue /(double) maxValue), 1.0f, 1.0f);
+
+        //clamp scale on x axis to be zero minimum
+        if(bar.localScale.x < 0)
+        {
+            bar.localScale = new Vector3(0.0f, 1.0f, 1.0f);
+        }
+    }
+
+}
